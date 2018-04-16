6eb4994435d361d79db178356c6703ff3fc34a0c
PrudhviRaj <pnookala@deloitte.com>
2018-04-16 09:32:39 +0530

    UI development

--- src/app/services/ua-http.service.ts
+++ src/app/services/ua-http.service.ts
@@ -10,28 +10,28 @@ export class RESTService {
     constructor(private http:HttpClient) {}
 
     getShopNowData(countryName) {
-        if(countryName) {
+        if(countryName === "French") {
             return this.http.get('http://ec2-18-217-68-79.us-east-2.compute.amazonaws.com:4503/bin/underarmour/search?searchTerm=portais,autre&locale=fr');
         }
         return this.http.get('http://ec2-18-217-68-79.us-east-2.compute.amazonaws.com:4503/bin/underarmour/search?searchTerm=downtime,outfitting,hooked');
     }
 
     getProductCardsData(countryName) {
-        if(countryName) {
+        if(countryName === "French") {
             return this.http.get('http://ec2-18-217-68-79.us-east-2.compute.amazonaws.com:4503/bin/underarmour/search?searchTerm=perpetual,chaussures,blouson&locale=fr');
         }
         return this.http.get('http://ec2-18-217-68-79.us-east-2.compute.amazonaws.com:4503/bin/underarmour/search?searchTerm=Hoodies,gear,customized');
     }
 
     getMegaMenu(countryName) {
-        if(countryName) {
+        if(countryName === "French") {
             return this.http.get('http://ec2-18-217-68-79.us-east-2.compute.amazonaws.com:4503/bin/underarmour/nav&locale=fr');
         }
         return this.http.get('http://ec2-18-217-68-79.us-east-2.compute.amazonaws.com:4503/bin/underarmour/nav');
     }
 
     getHeaderBanner(countryName) {
-        if(countryName) {
+        if(countryName === "French") {
             return this.http.get('http://ec2-18-217-68-79.us-east-2.compute.amazonaws.com:4503/bin/underarmour/search?searchTerm=lifts&locale=fr');
         }
         return this.http.get('http://ec2-18-217-68-79.us-east-2.compute.amazonaws.com:4503/bin/underarmour/search?searchTerm=lifts');
